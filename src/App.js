e7d8aa3d59ee281959ae296ba62f35a7bcf521c0
jonahBerlyne <jonshaber@gmail.com>
2022-01-06 15:33:00 -0500

    added item info

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import Navbar from "./Navbar";
 import Main from "./Main";
+import data from "./data";
 
 export default function App() {
   return (
     <div className="App">
       <Navbar/>
-      <Main/>
+      <Main data={data}/>
     </div>
   );
 }
--- src/Main.js
+++ src/Main.js
@@ -1,8 +1,27 @@
-export default function Main () {
+export default function Main ({data}) {
+
+ // For data:
+ // imageUrl on left
+ // on right, text columns
+ // col 1: location, googleMapsUrl
+ // col 2: title
+ // col 3: startDate-endDate
+ // col 4: description
+ console.log(data);
 
  return (
   <main>
-   Main section
+   {data.map(item => {
+    return (
+     <div key={item.id}>
+      <img src={`${item.imageUrl}`} alt="Mount Fuji"/>
+      <p>{item.location} <a href={`${item.googleMapsUrl}`}>View on Google Maps</a></p>
+      <h1>{item.title}</h1>
+      <h4>{item.startDate}-{item.endDate}</h4>
+      <p>{item.description}</p>
+     </div>
+    );
+   })}
   </main>
  );
 }
